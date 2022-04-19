f3e9ededda77e055fa665fa9138f59782f771bda
ChuWenFeng <1721306020@qq.com>
2022-04-19 18:20:27 +0800

    Interview

--- Interview/src/lib.rs
+++ Interview/src/lib.rs
@@ -40,15 +40,38 @@ pub fn rand_group(arr:&Vec<String>,k:usize)->Vec<Vec<String>>{
     group
 }
 
+/// 模拟分组选择
+fn rand_group_select(arr:&Vec<String>,k:usize)->Vec<Vec<String>>{
+    if k == 0{
+        return vec![arr.clone()];
+    }
+    if arr.len() == 0{
+        return vec![];
+    }
+    let mut ans = vec![vec![];k];
+    let mut plist = arr.clone();
+    let mut tail = arr.len();
+    let mut rng = rand::thread_rng();
+    for i in 0..plist.len(){
+        let r = rng.gen_range(0..tail);
+        tail-=1;
+        ans[i%k].push(plist[r].clone());
+        plist.swap(r, tail);
+    }
+
+
+    ans
+}
+
 
 #[cfg(test)]
 mod tests {
     use std::fmt::format;
 
-    use crate::rand_group;
+    use crate::{rand_group,rand_group_select};
 
     #[test]
-    fn test(){
+    fn test_range_group(){
 
         let mut arr = vec![];
         let people_num = 200;
@@ -87,6 +110,46 @@ mod tests {
             }
         }
 
+    }
+
+    #[test]
+    fn test_range_group_select(){
+        let mut arr = vec![];
+        let people_num = 200;
+        for _ in 0..people_num{
+            arr.push(gen_rand_string());
+        }
+        
+        {// 可以整除的情况   
+            let k = 20;
+            let group = rand_group_select(&arr, k);
+            let num_team = people_num/k;
+            for v in group{
+                assert!(v.len()==num_team);
+            }
+        }
+
+        {// 无法整除的情况
+            let k = 19;
+            let group = rand_group_select(&arr, k);
+            let num_team = people_num/k;
+            for v in group{
+                assert!((v.len()-num_team) <= 1);
+            }
+        }
+        {// 分组数为0的情况
+            let k = 0;
+            let group = rand_group_select(&arr, k);
+            assert!(group.len() == 1 && group[0].len() == arr.len());
+        }
+        {// 人数为0的情况
+            let k = 18;
+            let arr = vec![];
+            let group = rand_group_select(&arr, k);
+            for v in group{
+                assert!(v.len()==0);
+            }
+        }
     }
 
     fn gen_rand_string()->String{
